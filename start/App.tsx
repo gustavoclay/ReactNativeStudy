602e81dd51bda8cd25a0dec9023696a9632e3870
Gustavo Clay <gustavoclay@gmail.com>
2020-07-29 00:48:06 -0300

    https://docs.expo.io/tutorial/image/

--- start/App.tsx
+++ start/App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: '#888', fontSize: 18 }}>
+        To share a photo from your phone with a friend, just press the button below!</Text>
     </View>
   );
 }
