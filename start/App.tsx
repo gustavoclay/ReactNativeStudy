fca6dd9ac2d3644ee4d1f8b0488dee3c841caf2c
Gustavo Clay <gustavoclay@gmail.com>
2020-07-29 01:22:23 -0300

    https://docs.expo.io/tutorial/button/

--- start/App.tsx
+++ start/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: '#888', fontSize: 18 }}>
+
+      <Image source={require('./assets/logo.png')} style={styles.logo}></Image>
+
+      <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button below!</Text>
-    </View>
+
+    </View >
   );
 }
 
@@ -17,4 +21,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15
+  }
 });
